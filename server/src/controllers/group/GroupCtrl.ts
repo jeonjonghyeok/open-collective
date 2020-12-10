c8738a7a9e23f9ba8e4238522af028e6bbc8e2a3
Jonghyeok Ethan Jeon <dblockchain.5942@gmail.com>
2020-12-10 13:28:03 +0900

    donation group

--- server/src/controllers/group/GroupCtrl.ts
+++ server/src/controllers/group/GroupCtrl.ts
@@ -1,8 +1,9 @@
-import {BodyParams, Controller, Get, Post, Req, HeaderParams, Scope, ProviderScope} from "@tsed/common";
-import { Description,Required, Returns } from "@tsed/schema";
+import {BodyParams, Controller, Get, Post, Req, HeaderParams, Scope, ProviderScope, PathParams} from "@tsed/common";
+import { Description,Required, Returns, Status } from "@tsed/schema";
 import {Authenticate, Authorize} from "@tsed/passport";
 import {Group} from "../../models/group/Group";
 import {GroupService} from "../../services/group/GroupService";
+import {NotFound} from "@tsed/exceptions";
 
 // @Scope(ProviderScope.SINGLETON)
 @Controller({
@@ -10,6 +11,16 @@ import {GroupService} from "../../services/group/GroupService";
 export class GroupCtrl {
 	constructor(private groupService: GroupService) {
 
+	}
+	@Get("/:id")
+	@(Status(200,Group).Description("Success"))
+	async get(@Required() @PathParams("id") id:string): Promise<Group>{
+		const group= await this.groupService.find(id);
+
+		if(group)
+		return group;
+
+		throw new NotFound("Not Group");
 	}
 
     @Post("/")
